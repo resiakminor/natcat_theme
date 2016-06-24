20515a6a33b38d7bc0d37589babc0c7156cc9a57
Matthew Kaiser <mjkaiser10@gmail.com>
2016-06-24 14:30:46 -0400

    Smooth Scrolling and Nav highlighting

--- js/menu.js
+++ js/menu.js
@@ -1,34 +1,56 @@
+// Remove active class when click on logo
+$('.navbar-brand').click(function(){
+  $('.nav li.active').removeClass('active');
+});
+
+// Active class added on click
 $('#navbar > ul.navbar-nav li').click(function(e) {
     $('.nav li.active').removeClass('active');
     var $this = $(this);
     $this.addClass('active');
+
 });
 
-jQuery(function($){
-    $(document).on('mousewheel', function(e){
-        e.preventDefault();
+// Smooth scroll for in page links
+$(function(){
+    var target, scroll;
+
+    $("a[href*=#]:not([href=#])").on("click", function(e) {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            target = $(this.hash);
+            target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+
+            if (target.length) {
+                if (typeof document.body.style.transitionProperty === 'string') {
+                    e.preventDefault();
+                  
+                    var avail = $(document).height() - $(window).height();
+
+                    scroll = target.offset().top;
+                  
+                    if (scroll > avail) {
+                        scroll = avail;
+                    }
+
+                    $("html").css({
+                        "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
+                        "transition" : "1s ease-in-out"
+                    }).data("transitioning", true);
+                } else {
+                    $("html, body").animate({
+                        scrollTop: scroll
+                    }, 1000);
+                    return;
+                }
+            }
+        }
+    });
+
+    $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
+        if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
+            $(this).removeAttr("style").data("transitioning", false);
+            $("html, body").scrollTop(scroll);
+            return;
+        }
     });
 });
-
-$(document).ready(function(){
-  $(".anchorLink").click(function(e) {
-    e.preventDefault();
- 
-    anchorScroll( $(this), $($(this).attr("href")), 100 );
-  });
-});
- 
-function anchorScroll(this_obj, that_obj, base_speed) {
-  var this_offset = this_obj.offset();
-  var that_offset = that_obj.offset();
-  var offset_diff = Math.abs(that_offset.top - this_offset.top);
- 
-  var speed       = (offset_diff * base_speed) / 1000;
- 
-  $("html,body").animate({
-  scrollTop: that_offset.top
-}, {
-  duration: speed,
-  easing: "easeInOutSine"
-});
-}
