c14364a4b53852582040a05f04ddaefcd2eb10bd
Matthew Kaiser <mjkaiser10@gmail.com>
2016-07-28 15:28:22 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,58 +1,4 @@
-/* // Remove active class when click on logo
-$('.navbar-brand').click(function(){
-  $('.nav li.active').removeClass('active');
-});
-
-// Active class added on click
-$('#navbar > ul.navbar-nav li').click(function(e) {
-    $('.nav li.active').removeClass('active');
-    var $this = $(this);
-    $this.addClass('active');
-}); */
-
-// Smooth scroll for in page links
-/* $(function(){
-    var target, scroll;
-
-    $("a[href*=#]:not([href=#])").on("click", function(e) {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            target = $(this.hash);
-            target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
-
-            if (target.length) {
-                if (typeof document.body.style.transitionProperty === 'string') {
-                    e.preventDefault();
-                  
-                    var avail = $(document).height() - $(window).height();
-
-                    scroll = target.offset().top;
-                  
-                    if (scroll > avail) {
-                        scroll = avail;
-                    }
-
-                    $("html").css({
-                        "margin-top" : ( $(window).scrollTop() - scroll ) + "px",
-                        "transition" : "1s ease-in-out"
-                    }).data("transitioning", true);
-                } else {
-                    $("html, body").animate({
-                        scrollTop: scroll
-                    }, 1000);
-                    return;
-                }
-            }
-        }
-    });
-
-    $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function (e) {
-        if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
-            $(this).removeAttr("style").data("transitioning", false);
-            $("html, body").scrollTop(scroll);
-            return;
-        }
-    });
-}); */
+jQuery( document ).ready( function($){
 
 $(document).ready(function () {
     $(document).on("scroll", onScroll);
@@ -125,3 +71,5 @@ function onScroll(event){
         }
     });
 }
+
+});
